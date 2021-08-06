9bf1d26eb69f66407804513b3d050da851de7974
mgracioli <michel-rg@hotmail.com>
2021-08-05 22:29:28 -0300

    funcionando o básico

--- src/App.js
+++ src/App.js
@@ -9,35 +9,49 @@ function App() {
   const [operado,setOperado]=useState(false)
 
   //estilos
+  const cssConteiner = {
+    display:'flex',
+    justifyContent:'center',
+    alignItems:'center',
+    flexDirection:'column',
+    width:240,
+    border:'1px solid #000'
+  }
+
+  const cssBotoes = {
+    flexDirection:'row',
+    flexWrap:'wrap'
+  }
+
   const cssTela = {
     display:'flex',
-    paddingLeft:'20',
-    paddingRight:'20',
     justifyContent:'center',
     alignItems:'flex-start',
     flexDirection:'column',
-    backgroundColor:'#444',
-    width:'260'
+    backgroundColor:'#333',
+    width:240
   }
 
   const cssTelaOperacao = {
-    fontSize:25,
+    fontSize:'20px',
+    paddingLeft:10,
     color:'#fff',
-    height:20
+    height:10
   }
 
   const cssTelaResultado = {
-    fontSize:50,
+    fontSize:'45px',
+    paddingLeft:10,
     color:'#fff',
-    height:20
+    height:55
   }
 
   const cssBtn = {
-    fontSize:30,
-    height:75,
-    width:75,
-    padding:20,
-    backgroundColor:'#000',
+    fontSize:'30px',
+    height:'60px',
+    width:'60px',
+    padding:5,
+    backgroundColor:'#444',
     color:"#fff",
     borderColor:"#000",
     textAlign:'center',
@@ -57,7 +71,7 @@ function App() {
 
   const Btn = (label,onClick) => {
     return(
-      <button style={cssBtn} onClick={onClick}></button>
+      <button style={cssBtn} onClick={onClick}>{label}</button>
     )
   }
 
@@ -65,12 +79,74 @@ function App() {
   const addDigitoTela = (d) => {
     if((d=='+' || d=='-' | d=='*' | d=='/') && operado){
       
+      return
+    }
+    if(operado){
+      setValorTela(d)
+      setOperado(false)
+      return
+    }
+    const valorDigitadoNatela=valorTela+d //valorTela+d é o valor que já está na tela mais o proximo digito digitado (pode ser um operador ou um outro numero), o operado só será true quando eu tiver uma expressão completa (Ex.: 5+3), nesse caso, o eval() vai pegar essa expressão, executar ela e, daí, setar operado para true
+    setValorTela(valorDigitadoNatela)
+  }
+
+  const limparMemoria = () => {
+    setOperado(false)
+    setValorTela('')
+    setResultado(0)
+    setAcumulador(0)
+    return
+  }
+
+  const operacao = (operacao)=>{
+    if(operacao == 'bs'){
+      let vTela=valorTela
+
+      vTela = vTela.substring(0,(vTela.length-1))
+      setValorTela(vTela)
+      setOperado(false)
+      return
+    }
+    try{
+      const r = eval(valorTela)  //eval() faz o cálculo a partir da expressão quee stiver digitada na tela, ele avalia a expressão digitada e executa ela
+      setAcumulador(r)
+      setResultado(r)
+      setOperado(true)
+    }catch{
+      setResultado('Erro')
     }
   }
 
   return (
     <>
-      
+      <div style={cssConteiner}>
+        <h1>Calculadora</h1>
+
+        {Tela(valorTela,resultado)}
+
+        <div style={cssBotoes}>
+          {Btn('AC',limparMemoria)}
+          {Btn('(', () => {addDigitoTela('(')})}
+          {Btn(')', () => {addDigitoTela(')')})}
+          {Btn('/', () => {addDigitoTela('/')})}
+          {Btn('7', () => {addDigitoTela('7')})}
+          {Btn('8', () => {addDigitoTela('8')})}
+          {Btn('9', () => {addDigitoTela('9')})}
+          {Btn('*', () => {addDigitoTela('*')})}
+          {Btn('4', () => {addDigitoTela('4')})}
+          {Btn('5', () => {addDigitoTela('5')})}
+          {Btn('6', () => {addDigitoTela('6')})}
+          {Btn('-', () => {addDigitoTela('-')})}
+          {Btn('1', () => {addDigitoTela('1')})}
+          {Btn('2', () => {addDigitoTela('2')})}
+          {Btn('3', () => {addDigitoTela('3')})}
+          {Btn('+', () => {addDigitoTela('+')})}
+          {Btn('0', () => {addDigitoTela('0')})}
+          {Btn('.', () => {addDigitoTela('.')})}
+          {Btn('<', () => {operacao('bs')})}
+          {Btn('=', () => {operacao('=')})}
+        </div>
+      </div>
     </>
   );
 }
